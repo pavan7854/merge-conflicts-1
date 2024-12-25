270d18520764cfba9de3ad01779f2f62f8460b8f
Pavan Uthsara <uthsarapavan@gmail.com>
2024-12-25 17:19:11 +0530

    updated hello.py v2.0

--- hello.py
+++ hello.py
@@ -3,7 +3,6 @@ age = 18
 if age > 18:
     print('adult')
 else :
-    print('child should be a change')
+    print('child with a big ice cream')
 
 print("let's demonstrate merge conflicts")   
-print("This is new line with no conflicts!")
